dd643ff3ae6b4599bbf6e2a6d50c8ed472c901d0
ENDHRAJITH <endhrajiththiyagu@gmail.com>
2026-04-26 00:05:31 +0530

    end and agalyaa:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
         </div>
         <div><div className="fch">Navigate</div><ul className="fll"><li><a href="#author">About the Author</a></li><li><a href="#books">Books &amp; Guides</a></li><li><a href="#consultation">Consultation</a></li><li><a href="#testimonials">Testimonials</a></li></ul></div>
         <div><div className="fch">Catalogue</div><ul className="fll"><li><a href="#books">All Books</a></li><li><a href="#books">Study Guides</a></li><li><a href="#books">Question Banks</a></li><li><a href="#consultation">Sessions</a></li></ul></div>
-        <div><div className="fch">Contact</div><ul className="fll"><li><a href="mailto:hello@bookshelf.in"> gnareshkumar94@gmail.com</a></li><li><a href="tel:+919025443254">+91 9025443254</a></li><li><a href="#">Chennai, Tamil Nadu</a></li><li><a href="#contact">Send a Message</a></li></ul></div>
+        <div><div className="fch">Contact</div><ul className="fll"><li><a href="mailto: gnareshkumar94@gmail.com"> gnareshkumar94@gmail.com</a></li><li><a href="tel:+919025443254">+91 9025443254</a></li><li><a href="#">Chennai, Tamil Nadu</a></li><li><a href="#contact">Send a Message</a></li></ul></div>
       </div>
       <div className="fbb"><span className="fcp">© 2025 Bookshelf · Naresh Kumar · All Rights Reserved</span><div className="fleg"><a href="#">Privacy Policy</a><a href="#">Terms of Use</a><a href="#">Refund Policy</a></div></div>
     </footer>
